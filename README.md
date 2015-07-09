ffe5d42aca57147ee73e415501ebd4b9acb2f842
tabeyti <tabeyti@blizzard.com>
2015-07-08 18:56:20 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,47 @@
+# [Personal Timeline]
+
+Personal timeline is a timeline application directed towards chronicling your own personal history.
+
+## Features
+
+Blank
+
+## To Do
+* Create custom item template in vis.js.
+* Create item/event JSON structure.
+** May include item display panel data as well.
+* Determine how new items are going to be created:
+** Double click?
+** Dialogue window?
+** Edit pane in Item Display Window?
+** Edit button for editing slider data?
+* Item Display Window
+** Add event listener for item selection/click.
+** Create alternate panel above slider
+** Update panel in slider after item event click.
+* Create Overview Panel for Navigation/Editing
+
+* Structure
+** Period
+** Event
+** Day
+
+## Browser support
+
+* Chrome *(latest 2)*
+* Firefox *(latest 2)*
+* Internet Explorer 8+
+* Opera *(latest 2)*
+* Safari *(latest 2)*
+
+## Documentation
+
+None yet.
+
+## Contributing
+
+None yet.
+
+## License
+
+The code is available under the [MIT license](LICENSE.txt).
---
+++ src/js/app.ctrl.js
@@ -0,0 +1,57 @@
+var app = angular.module("app", ["xeditable"]);
+
+app.run(function(editableOptions) {
+  editableOptions.theme = 'bs3';
+});
+
+app.controller("MainController", function($scope, $http){
+    var vm = this;
+    vm.title = 'Personal Timeline';
+    vm.content = 'asdfasdf';
+
+  var items = new vis.DataSet({
+    type: { start: 'ISODate', end: 'ISODate' }
+  });
+
+  // add items to the DataSet
+  items.add([
+    {id: 1, content: 'item 1<br>start', start: '2014-01-23'},
+    {id: 2, content: 'item 2', start: '2014-01-18'},
+    {id: 3, content: 'item 3', start: '2014-01-21'},
+    {id: 4, content: 'item 4', start: '2014-01-19', end: '2014-01-24'},
+    {id: 5, content: 'item 5', start: '2014-01-26'}
+  ]);
+
+  var container = document.getElementById('visualization');
+  var options = {
+    height: '300px',
+    multiselect: false,
+    // allow manipulation of items
+    editable: true,
+    showCurrentTime: true
+  };
+  var timeline = new vis.Timeline(container, items, options);
+
+  // register item select listener, so when an item is clicked, content
+  // is displayed above the timeline
+  timeline.on('select', function(stuff)
+  {
+    $http.get("https://dl.dropboxusercontent.com/u/9040177/test.json")
+      .success(function(response)
+      {
+        console.log(response);
+      });
+
+    vm.content = stuff.items[0];
+    $scope.$apply();
+  });
+
+});
+
+app.controller("ElementDisplayController", function($scope, $filter)
+{
+  $scope.itemDisplay =
+  {
+    desc: 'Bacon is king\nyes'
+  };
+});
