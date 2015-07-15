c9d758c5f461acbc5f34cda0b7c2fc8c3b9ed205
tabeyti <tabeyti@blizzard.com>
2015-07-14 19:03:56 -0700

    Added retrieval for json data from wampserver.
    Updates top window with item data once item is clicked.

--- js/app.js
+++ js/app.js
@@ -30,13 +30,7 @@ app.controller("MainController", function($scope, $http, sharedService){
   });
 
   // add items to the DataSet
-  items.add([
-    {id: 1, content: 'item 1<br>start', start: '2014-01-23'},
-    {id: 2, content: 'item 2', start: '2014-01-18'},
-    {id: 3, content: 'item 3', start: '2014-01-21'},
-    {id: 4, content: 'item 4', start: '2014-01-19', end: '2014-01-24'},
-    {id: 5, content: 'item 5', start: '2014-01-26'}
-  ]);
+  items.add([]);
 
   var container = document.getElementById('visualization');
   var options = {
@@ -48,25 +42,27 @@ app.controller("MainController", function($scope, $http, sharedService){
   };
   var timeline = new vis.Timeline(container, items, options);
 
+  $http.get("http://172.248.208.18:8000/ptl/process.php?method=getTimeline")
+    .success(function(response) {
+      console.log(response);
+      items.clear();
+      items.add(response);
+      timeline.fit();
+    });
+
   // register item select listener, so when an item is clicked, content
   // is displayed above the timeline
   timeline.on('select', function(stuff)
   {
-    $http.get("https://dl.dropboxusercontent.com/u/9040177/test.json")
-      .success(function(response)
-      {
-        console.log(response);
-        items.clear();
-        items.add(response);
-        timeline.fit();
+    console.log('Item clicked: ' + stuff);
+    $http.get("http://172.248.208.18:8000/ptl/process.php?method=getItem&id="+ stuff.items[0])
+      .success(function(response) {
+        vm.content = 'Id: ' + stuff.items[0];
+        sharedService.msg = response['journal'];
+        sharedService.publishId();
+        $scope.$apply();
       });
-
-    vm.content = stuff.items[0];
-    sharedService.msg = stuff.items[0];
-    sharedService.publishId();
-    $scope.$apply();
   });
-
 });
 
 app.controller("ItemDisplayController", function($scope, $filter, sharedService){
