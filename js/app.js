bc9c71e74a5bcac98cc2d6f412d0ad57ce3bb255
tabeyti <tabeyti@blizzard.com>
2015-07-15 19:11:53 -0700

    Added contextMenu to timeline
    Added delete ability for items via contextMenu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("app", ["xeditable"]);
+var app = angular.module("app", ["xeditable", "ui.bootstrap.contextMenu"]);
 
 /*
   Shared services between app controllers
@@ -16,9 +16,9 @@ app.factory('sharedService', function($rootScope){
   return sharedService;
 });
 
-/*
-  CONTROLLERS
-*/
+////////////////////////////////////////////////////////////////////////////////
+// controllers
+////////////////////////////////////////////////////////////////////////////////
 app.controller("MainController", function($scope, $http, sharedService){
   var vm = this;
   vm.title = 'Personal Timeline';
@@ -42,28 +42,68 @@ app.controller("MainController", function($scope, $http, sharedService){
   };
   var timeline = new vis.Timeline(container, items, options);
 
-  $http.get("http://172.248.208.18:8000/ptl/process.php?method=getTimeline")
-    .success(function(response) {
-      console.log(response);
-      items.clear();
-      items.add(response);
-      timeline.fit();
+  // $http.get("http://172.248.208.18:8000/ptl/process.php?method=getTimeline")
+  //   .success(function(response) {
+  //     console.log(response);
+  //     items.clear();
+  //     items.add(response);
+  //     timeline.fit();
+  //   });
+
+  function saveData() {
+    var data = items.get({
+      type: {
+        start: 'ISODate',
+        end: 'ISODate'
+      }
     });
+    return JSON.stringify(data, null, 2);
+  };
 
   // register item select listener, so when an item is clicked, content
   // is displayed above the timeline
-  timeline.on('select', function(stuff)
-  {
+  timeline.on('select', function(stuff) {
     console.log('Item clicked: ' + stuff);
-    $http.get("http://172.248.208.18:8000/ptl/process.php?method=getItem&id="+ stuff.items[0])
-      .success(function(response) {
-        vm.content = 'Id: ' + stuff.items[0];
-        sharedService.msg = response['journal'];
-        sharedService.publishId();
-        $scope.$apply();
-      });
+    // $http.get("http://172.248.208.18:8000/ptl/process.php?method=getItem&id="+ stuff.items[0])
+    //   .success(function(response) {
+    //     vm.content = 'Id: ' + stuff.items[0];
+    //     sharedService.msg = response['journal'];
+    //     sharedService.publishId();
+    //     $scope.$apply();
+    //   });
   });
+  timeline.on('contextmenu', function(props) {
+      timeline.setSelection(props.item);
+    });
+
+  // context-menu
+  $scope.menuOptions = function(item) {
+    // item context menu
+    if (timeline.getSelection().length == 0) {
+      return [
+        ['Add Range', function ($itemScope) {
+          //items.add({"content": "blank"});
+        }],
+        ['Add Box', function ($itemScope) {
+
+        }],
+        ['Add Point', function ($itemScope) {
+
+        }]
+      ];
+    }
+    // timeline background context menu
+    else {
+      return [
+        ['Delete', function ($itemScope) {
+          items.remove(timeline.getSelection()[0]);
+        }]
+      ];
+    }
+  };
 });
+
+
 
 app.controller("ItemDisplayController", function($scope, $filter, sharedService){
   $scope.$on('handlePublish', function(){
